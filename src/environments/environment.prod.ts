4a47750da7348bcde1179b65e40437dc4bb6c157
Daniel Luna <dluna@theworkshop.com>
2021-10-06 18:50:59 +0200

    supabase key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  supabase: {
+    key: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzMzM1NTkxOSwiZXhwIjoxOTQ4OTMxOTE5fQ.nVX6lZ-zuUQCwp2FjkBJdiZKhY0LPe-nDnScAETK1O8",
+    url: "https://edyepibtnkccgdrkqcix.supabase.co",
+    storage: {
+      url: "https://edyepibtnkccgdrkqcix.supabase.in/storage/v1/object/public/"
+    }
+  }
 };
